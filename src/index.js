4afb256bc7331422ae7b89b3936ed602f253c2e0
Yura Goldman <yura_goldman@mail.ru>
2022-04-28 16:37:21 +0300

    parallax minerals, benefits, services + ecology cards

--- src/index.js
+++ src/index.js
@@ -39,74 +39,44 @@ document.addEventListener("DOMContentLoaded", () => {
   }();
 
   // Параллакс
-  +function parallaxPlastic() {
+  const screen = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+
+  const center = {
+    x: screen.width / 2,
+    y: screen.height / 2
+  }
+
+  parallaxPlastic(center);
+  parallaxStats(center);
+  parallaxServices(center);
+  parallaxBenefits(center);
+  parallaxMinerals(center);
+
+  function parallaxPlastic(center) {
     let parallax = {
-      plastic: {
+      el: null,
+      bg: {
         el: null,
-        bg: {
-          el: null,
-          max: null
-        },
-        obj: {
-          el: null,
-          max: 50
-        }
+        max: null
       },
-      stats: {
+      obj: {
         el: null,
-        bg: {
-          el: null,
-          max: null
-        },
-        first: {
-          el: null,
-          max: null
-        },
-        second: {
-          el: null,
-          max: null
-        }
+        max: 50
       }
     }
 
-    parallax.plastic.el = document.querySelector('[data-parallax-plastic]');
-    parallax.plastic.obj.el = document.querySelector('[data-parallax-plastic-obj]');
-    parallax.plastic.bg.el = document.querySelector('[data-parallax-plastic-bg]');
-
-    parallax.plastic.bg.el.addEventListener('load', () => {
-      parallax.plastic.bg.max = (parallax.plastic.bg.el.getBoundingClientRect().width - parallax.plastic.el.getBoundingClientRect().width) / 2;
-    })
-
-    parallax.stats.el = document.querySelector('[data-parallax-stats]');
-    parallax.stats.first.el = document.querySelector('[data-parallax-stats-first]');
-    parallax.stats.first.el.addEventListener('load', () => {
-      parallax.stats.first.max = (parallax.stats.first.el.getBoundingClientRect().width - parallax.stats.el.getBoundingClientRect().width) / 4;
-    })
-    
-    parallax.stats.second.el = document.querySelector('[data-parallax-stats-second]');
-    parallax.stats.second.el.addEventListener('load', () => {
-      parallax.stats.second.max = (parallax.stats.second.el.getBoundingClientRect().width - parallax.stats.el.getBoundingClientRect().width) / 4;
-    })
-    
-    parallax.stats.bg.el = document.querySelector('[data-parallax-stats-bg]');
-    parallax.stats.bg.el.addEventListener('load', () => {
-      parallax.stats.bg.max = (parallax.stats.bg.el.getBoundingClientRect().width - parallax.stats.el.getBoundingClientRect().width) / 2;
-    })
-
-    const screen = {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }
+    parallax.el = document.querySelector('[data-parallax-plastic]');
+    parallax.obj.el = document.querySelector('[data-parallax-plastic-obj]');
+    parallax.bg.el = document.querySelector('[data-parallax-plastic-bg]');
+    parallax.bg.max = (parallax.bg.el.getBoundingClientRect().width - parallax.el.getBoundingClientRect().width) / 2;
 
     document.addEventListener('mousemove', event => {
       const mouse = {
         x: event.clientX === 0 ? event.clientX = 1 : event.clientX,
         y: event.clientY === 0 ? event.clientY = 1 : event.clientY
-      }
-
-      const center = {
-        x: screen.width / 2,
-        y: screen.height / 2
       }
 
       const current = {
@@ -121,25 +91,203 @@ document.addEventListener("DOMContentLoaded", () => {
 
       requestAnimationFrame(() => {
         requestAnimationFrame(() => {
-          if (parallax.plastic.bg.el !== null) {
-            parallax.plastic.bg.el.style.transform = `translate3d(calc(-50% + ${parallax.plastic.bg.max * mod.x * -1}px), calc(-50% + ${parallax.plastic.bg.max * mod.y * -1}px), 1px)`;
-          }
-
-          if (parallax.plastic.obj.el !== null) {
-            parallax.plastic.obj.el.style.transform = `translate3d(${parallax.plastic.obj.max * mod.x * -1}px, ${parallax.plastic.obj.max * mod.y * -1}px, 1px)`;
-          }
-
-          if (parallax.stats.bg.el !== null) {
-            parallax.stats.bg.el.style.transform = `translate3d(calc(-50% + ${parallax.stats.bg.max * mod.x * -1}px), calc(-50% + ${parallax.stats.bg.max * mod.y * -1}px), 1px)`;
-          }
-          if (parallax.stats.first.el !== null) {
-            parallax.stats.first.el.style.transform = `translate3d(calc(-50% + ${parallax.stats.second.max * mod.x * -1}px), ${parallax.stats.second.max * mod.y * -1}px, 1px)`;
-          }
-          if (parallax.stats.second.el !== null) {
-            parallax.stats.second.el.style.transform = `translate3d(calc(-50% + ${parallax.stats.second.max * mod.x * -1}px + 50px), ${parallax.stats.second.max * mod.y * -1}px, 1px)`;
-          }
+          parallax.bg.el.style.transform = `translate3d(calc(-50% + ${parallax.bg.max * mod.x * -1}px), calc(-50% + ${parallax.bg.max * mod.y * -1}px), 1px)`;
+          parallax.obj.el.style.transform = `translate3d(${parallax.obj.max * mod.x * -1}px, ${parallax.obj.max * mod.y * -1}px, 1px)`;
         })
       })
     });
-  }();
+  }
+
+  function parallaxStats(center) {
+    let parallax = {
+      el: null,
+      bg: {
+        el: null,
+        max: null
+      },
+      first: {
+        el: null,
+        max: null
+      },
+      second: {
+        el: null,
+        max: null
+      }
+    }
+
+    parallax.el = document.querySelector('[data-parallax-stats]');
+    parallax.first.el = document.querySelector('[data-parallax-stats-first]');
+    parallax.first.max = (parallax.first.el.getBoundingClientRect().width - parallax.el.getBoundingClientRect().width) / 4;
+    
+    parallax.second.el = document.querySelector('[data-parallax-stats-second]');
+    parallax.second.max = (parallax.second.el.getBoundingClientRect().width - parallax.el.getBoundingClientRect().width) / 4;
+    
+    parallax.bg.el = document.querySelector('[data-parallax-stats-bg]');
+    parallax.bg.max = (parallax.bg.el.getBoundingClientRect().width - parallax.el.getBoundingClientRect().width) / 2;
+
+    document.addEventListener('mousemove', event => {
+      const mouse = {
+        x: event.clientX === 0 ? event.clientX = 1 : event.clientX,
+        y: event.clientY === 0 ? event.clientY = 1 : event.clientY
+      }
+
+      const current = {
+        x: center.x - mouse.x,
+        y: center.y - mouse.y
+      }
+
+      const mod = {
+        x: current.x / center.x,
+        y: current.y / center.y
+      }
+
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          parallax.bg.el.style.transform = `translate3d(calc(-50% + ${parallax.bg.max * mod.x * -1}px), calc(-50% + ${parallax.bg.max * mod.y * -1}px), 1px)`;
+          parallax.first.el.style.transform = `translate3d(calc(-50% + ${parallax.second.max * mod.x * -1}px), ${parallax.second.max * mod.y * -1}px, 1px)`;
+          parallax.second.el.style.transform = `translate3d(calc(-50% + ${parallax.second.max * mod.x * -1}px + 50px), ${parallax.second.max * mod.y * -1}px, 1px)`;
+        })
+      })
+    });
+  }
+
+  function parallaxServices(center) {
+    let parallax = {
+      el: null,
+      car: {
+        el: null,
+        max: 10
+      },
+      app: {
+        el: null,
+        max: 40
+      },
+      after: {
+        el: null,
+        max: 35
+      },
+      bg: {
+        el: null,
+        max: null
+      }
+    }
+
+    parallax.el = document.querySelector('[data-parallax-service]');
+    parallax.car.el = document.querySelector('[data-parallax-service-car]');
+    parallax.app.el = document.querySelector('[data-parallax-service-app]');
+    parallax.after.el = document.querySelector('[data-parallax-service-app-after]');
+    parallax.bg.el = document.querySelector('[data-parallax-service-bg]');
+    parallax.bg.max = (parallax.bg.el.getBoundingClientRect().width - parallax.el.getBoundingClientRect().width) / 2;
+    parallax.car.max = parallax.bg.max * 2;
+    parallax.app.max = parallax.bg.max * 5;
+    parallax.after.max = parallax.bg.max * 4;
+
+    document.addEventListener('mousemove', event => {
+      const mouse = {
+        x: event.clientX === 0 ? event.clientX = 1 : event.clientX,
+        y: event.clientY === 0 ? event.clientY = 1 : event.clientY
+      }
+
+      const current = {
+        x: center.x - mouse.x,
+        y: center.y - mouse.y
+      }
+
+      const mod = {
+        x: current.x / center.x,
+        y: current.y / center.y
+      }
+
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          parallax.car.el.style.transform = `translate3d(${parallax.car.max * mod.x * -1}px, ${parallax.car.max * mod.y * -1}px, 1px)`;
+          parallax.app.el.style.transform = `translate3d(${parallax.app.max * mod.x * -1}px, ${parallax.app.max * mod.y * -1}px, 1px)`;
+          parallax.after.el.style.transform = `translate3d(calc(-50% + ${parallax.after.max * mod.x * -1}px), calc(-50% + ${parallax.after.max * mod.y * -1}px), 1px)`;
+          parallax.bg.el.style.transform = `translate3d(calc(-50% + ${parallax.bg.max * mod.x * -1}px), calc(-50% + ${parallax.bg.max * mod.y * -1}px), 1px)`;
+        })
+      })
+    });
+  }
+
+  function parallaxBenefits(center) {
+    let parallax = {
+      el: null,
+      bg: {
+        el: null,
+        max: null
+      }
+    }
+
+    parallax.el = document.querySelector('[data-parallax-benefits]');
+    parallax.bg.el = document.querySelector('[data-parallax-benefits-bg]');
+    parallax.bg.max = (parallax.bg.el.getBoundingClientRect().width - parallax.el.getBoundingClientRect().width) / 2;
+
+    document.addEventListener('mousemove', event => {
+      const mouse = {
+        x: event.clientX === 0 ? event.clientX = 1 : event.clientX,
+        y: event.clientY === 0 ? event.clientY = 1 : event.clientY
+      }
+
+      const current = {
+        x: center.x - mouse.x,
+        y: center.y - mouse.y
+      }
+
+      const mod = {
+        x: current.x / center.x,
+        y: current.y / center.y
+      }
+
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          parallax.bg.el.style.transform = `translate3d(calc(-50% + ${parallax.bg.max * mod.x * -1}px), calc(-50% + ${parallax.bg.max * mod.y * -1}px), 1px)`;
+        })
+      })
+    });
+  }
+
+  function parallaxMinerals(center) {
+    let parallax = {
+      el: null,
+      bg: {
+        el: null,
+        max: null
+      },
+      obj: {
+        el: null,
+        max: null
+      }
+    }
+
+    parallax.el = document.querySelector('[data-parallax-minerals]');
+    parallax.bg.el = document.querySelector('[data-parallax-minerals-bg]');
+    parallax.bg.max = (parallax.bg.el.getBoundingClientRect().width - parallax.el.getBoundingClientRect().width) / 2;
+    parallax.obj.el = document.querySelector('[data-parallax-minerals-obj]');
+    parallax.obj.max = (parallax.obj.el.getBoundingClientRect().width - parallax.el.getBoundingClientRect().width) / 6;
+
+
+    document.addEventListener('mousemove', event => {
+      const mouse = {
+        x: event.clientX === 0 ? event.clientX = 1 : event.clientX,
+        y: event.clientY === 0 ? event.clientY = 1 : event.clientY
+      }
+
+      const current = {
+        x: center.x - mouse.x,
+        y: center.y - mouse.y
+      }
+
+      const mod = {
+        x: current.x / center.x,
+        y: current.y / center.y
+      }
+
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          parallax.bg.el.style.transform = `translate3d(calc(-50% + ${parallax.bg.max * mod.x * -1}px), calc(-50% + ${parallax.bg.max * mod.y * -1}px), 1px)`;
+          parallax.obj.el.style.transform = `translate3d(calc(-50% + ${parallax.obj.max * mod.x * -1}px), calc(-50% + ${parallax.obj.max * mod.y * -1}px), 1px)`;
+        })
+      })
+    });
+  }
 })
